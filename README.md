5b0171286377abb4dd2a2e0071455c37b377d4d4
Michael Taylor <mjt145@gmail.com>
2021-09-24 20:32:39 +0100

    updated readme and changed structure

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 A simple Marketing Mix Modeling library in Python
 
+## about
+
+Most Marketing Mix Models (MMM) are built in Excel, because it's simpler than running a script. But there are things you can't easily do in Excel, like testing 1,130 models to see which one works best. Hommmer is designed to be as simple as possible, while taking advantage of the power of Python. We'll probably annoy the statisticians by over-simplifying things (D'oh!), but we hope you like the results.
+
+## author
+
 Created by [@hammer_mt](https://twitter.com/hammer_mt) under the MIT license.
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import pathlib
 from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
-VERSION = '0.0.0.1'
-PACKAGE_NAME = 'hommmer'
-AUTHOR = 'mike taylor'
-AUTHOR_EMAIL = 'mike@saxifrage.xyz'
-URL = 'https://github.com/hammer-mt/hommmer'
-LICENSE = 'MIT'
-DESCRIPTION = 'A simple Marketing Mix Modeling library in Python'
+VERSION = "0.0.0.2"
+PACKAGE_NAME = "hommmer"
+AUTHOR = "mike taylor"
+AUTHOR_EMAIL = "mike@saxifrage.xyz"
+URL = "https://github.com/hammer-mt/hommmer"
+LICENSE = "MIT"
+DESCRIPTION = "A simple Marketing Mix Modeling library in Python"
 LONG_DESCRIPTION = (HERE / "README.md").read_text()
 LONG_DESC_TYPE = "text/markdown"
 INSTALL_REQUIRES = [
-    'numpy',
-    'pandas',
-    'matplotlib',
-    'statsmodels'
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "statsmodels"
 ]
 setup(name=PACKAGE_NAME,
     version=VERSION,
@@ -26,5 +26,6 @@ setup(name=PACKAGE_NAME,
     author_email=AUTHOR_EMAIL,
     url=URL,
     install_requires=INSTALL_REQUIRES,
-    packages=find_packages()
+    package_dir={"": "src"},
+    packages=find_packages(where="src")
     )
