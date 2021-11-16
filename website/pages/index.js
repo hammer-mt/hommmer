b83db7688a43920dc8b3960756a38f28887e5c13
Michael Taylor <mjt145@gmail.com>
2021-11-16 23:30:22 +0000

    update copy

--- website/pages/index.js
+++ website/pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
           >
             <h3 className="text-2xl font-bold">Tutorials &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Marketing Mix Modeling in simulator-based courses.
+              Learn about marketing mix modeling in simulator-based courses.
             </p>
           </a>
 
